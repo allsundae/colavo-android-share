81ba3ff039f4443b4164e77be438f5da607e5ba8
allsundae <1.infini.loop@gmail.com>
2017-08-14 21:38:26 +0900

    Fix Build error

--- app/src/main/java/com/colavo/android/App.kt
+++ app/src/main/java/com/colavo/android/App.kt
@@ -1,6 +1,7 @@
 package com.colavo.android
 
 import android.app.Application
+import com.google.firebase.auth.FirebaseAuth
 import com.colavo.android.di.app.AppComponent
 import com.colavo.android.di.app.AppModule
 import com.colavo.android.di.app.DaggerAppComponent
@@ -8,8 +9,17 @@ import com.colavo.android.di.event.EventComponent
 import com.colavo.android.di.event.EventModule
 import com.colavo.android.di.salons.SalonsComponent
 import com.colavo.android.di.salons.SalonsModule
+import com.colavo.android.di.firebase.FirebaseModule
+import com.colavo.android.di.net.NetModule
 import com.colavo.android.di.session.SessionComponent
 import com.colavo.android.di.session.SessionModule
+import com.colavo.android.repositories.salons.SalonsRepository
+import com.colavo.android.repositories.salons.datasource.SalonsDataSourceImpl
+import com.colavo.android.repositories.session.SessionRepository
+import com.colavo.android.repositories.session.datasource.SessionDataSourceImpl
+import com.colavo.android.utils.HandleUtils
+import com.colavo.android.utils.Logger
+
 
 /**
  * Created by RUS on 12.07.2016.
