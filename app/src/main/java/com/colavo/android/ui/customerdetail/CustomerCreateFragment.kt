f9cd1cbbb0b09170be5b97dff7f47e271a6b7a32
allsundae <1.infini.loop@gmail.com>
2018-01-21 05:02:43 +0900

    + Image uploader (for Profile, Memo) attached

--- app/src/main/java/com/colavo/android/ui/customerdetail/CustomerCreateFragment.kt
+++ app/src/main/java/com/colavo/android/ui/customerdetail/CustomerCreateFragment.kt
@@ -1,5 +1,6 @@
 package com.colavo.android.ui.customerdetail
 
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -18,6 +19,25 @@ import kotlinx.android.synthetic.main.customer_create.*
 import android.support.design.widget.BottomSheetBehavior
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.view.inputmethod.InputMethodManager
+import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.storage.UploadTask
+import com.google.firebase.storage.OnProgressListener
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import kotlin.reflect.jvm.internal.impl.load.java.lazy.ContextKt.child
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.FirebaseStorage
+import android.app.ProgressDialog
+import android.content.ContentValues.TAG
+import android.provider.MediaStore
+import android.graphics.Bitmap
+import android.net.Uri
+import android.util.Log
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CustomerCreateFragment : BaseFragment(), CustomerCreateView {
@@ -25,6 +45,7 @@ class CustomerCreateFragment : BaseFragment(), CustomerCreateView {
     lateinit var customerCreatePresenter: CustomerCreatePresenterImpl
 
     var imageURL : ImageUrl = ImageUrl("","")
+    private var filePath: Uri? = null
 
     override fun getLayout() = R.layout.customer_create
 
@@ -60,6 +81,18 @@ class CustomerCreateFragment : BaseFragment(), CustomerCreateView {
                         // no op
                     }
                 })
+        create_customer_image.setOnClickListener({ v -> dismissFragment() })
+
+        create_customer_image.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0)
+            }
+        })
+
+
     }
 
     private fun dismissFragment() {
@@ -83,6 +116,7 @@ class CustomerCreateFragment : BaseFragment(), CustomerCreateView {
         else{
             if (input_phone.isValid()) {
                 myInternationalNumber = input_phone.getNumber()
+                uploadFile()
                 customerCreatePresenter.createCustomer(salon.id, input_name.text.toString(), myInternationalNumber, imageURL) //input_phone.text.toString()
             }
             else
@@ -91,6 +125,66 @@ class CustomerCreateFragment : BaseFragment(), CustomerCreateView {
             }
         }
 
+    }
+
+    //결과 처리
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //request코드가 0이고 OK를 선택했고 data에 뭔가가 들어 있다면
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            filePath = data!!.data
+            Log.d(TAG, "uri:" + filePath.toString())
+            try {
+                //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
+                val bitmap = MediaStore.Images.Media.getBitmap( (context!!.applicationContext as App).getContentResolver(), filePath)
+                create_customer_image.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    //upload the file
+    private fun uploadFile() {
+        //업로드할 파일이 있으면 수행
+        if (filePath != null) {
+            //업로드 진행 Dialog 보이기
+            val progressDialog = ProgressDialog(context)
+            progressDialog.setTitle("업로드중...")
+            progressDialog.show()
+
+            //storage
+            val storage = FirebaseStorage.getInstance()
+
+            //Unique한 파일명을 만들자.
+            val formatter = SimpleDateFormat("yyyyMMHH_mmss")
+            val now = Date()
+            val filename = formatter.format(now) + ".png"
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            val storageRef = storage.getReferenceFromUrl("gs://jhone-364e5.appspot.com").child("images/" + filename)
+            //올라가거라...
+            storageRef.putFile(filePath!!)
+                    //성공시
+                    .addOnSuccessListener {
+                        progressDialog.dismiss() //업로드 진행 Dialog 상자 닫기
+                        showToast ("Upload completed.") //getString(R.string.success_create_customer)
+
+                    }
+                    //실패시
+                    .addOnFailureListener {
+                        progressDialog.dismiss()
+                        showToast ("Upload failed.") //getString(R.string.success_create_customer)
+                    }
+                    //진행중
+                    .addOnProgressListener { taskSnapshot ->
+                        val progress = (100 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount).toDouble()//이걸 넣어 줘야 아랫줄에 에러가 사라진다. 넌 누구냐?
+                        //dialog에 진행률을 퍼센트로 출력해 준다
+                        progressDialog.setMessage("Uploaded " + progress.toInt() + "% ...")
+                    }
+        } else {
+            showToast ("Select photo first.") //getString(R.string.success_create_customer)
+        }
     }
 
 
